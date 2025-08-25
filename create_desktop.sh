02661f3ca8dc8d0d3522d9c93395b8beb211a3bb
Izz4c <izzac.dev@gmail.com>
2025-08-25 11:50:35 +0530

    Update create_desktop.sh

--- create_desktop.sh
+++ create_desktop.sh
@@ -2,7 +2,7 @@ APP_NAME="Raptor"
 DESKTOP_FILE="$HOME/.local/share/applications/raptor.desktop"
 ICON="system-run"  # You can change this to any valid icon name or path
 TERMINAL="true"           # Set to false if you don't want a terminal window
-SCRIPT_PATH="wine $HOME/Downloads/raptor_avalonia/publish/raptor.exe"
+SCRIPT_PATH="env WINEPREFIX=$HOME/.wine64/ $HOME/Downloads/raptor_avalonia/publish/raptor.exe"
 # Create the .desktop file
 cat > "$DESKTOP_FILE" <<EOL
 [Desktop Entry]
